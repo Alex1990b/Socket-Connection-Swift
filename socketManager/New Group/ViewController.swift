d1776231047326136be3cfc33c9f5221981dad43
Alex1990b <sashabondar999@gmail.com>
2018-09-26 16:44:31 +0300

    Update ViewController.swift

--- socketManager/New Group/ViewController.swift
+++ socketManager/New Group/ViewController.swift
@@ -9,53 +9,36 @@
 import UIKit
 import NetworkExtension
 
-class ViewController: UIViewController, WiFiConnectable {
+final class ViewController: UIViewController {
+    
+   @IBOutlet private weak var testLabel: UILabel!  
     
    private var socketConnector: SocketConnectionProtocol!
-    private var dataSocket: DataSocket!
+   private var dataSocket: DataSocket!
    
-    @IBOutlet weak var testLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         socketConnector = SocketDataManager(with: self)
         dataSocket = DataSocket(ipAddress: "10.10.0.62", port: "50000")
         socketConnector.connectWith(socket: dataSocket)
         socketConnector.open()
-        
-
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    @IBAction func send(_ sender: UIButton) {
-        socketConnector.send(message: "TEST")
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
+    }    
 }
+
 extension ViewController: PresenterProtocol{
     func updateStatusViewWith(status: String) {
-        
     }
     
-    
-    func resetUIWithConnection(status: Bool){
-        
-           print(status)
-     
+    func resetUIWithConnection(status: Bool) {
     }
    
-    func update(message: String){
-        
-     testLabel.text = message
-        
+    func update(message: String){ }
+}
+
+private extension ViewController {
+    
+      @IBAction func send(_ sender: UIButton) {
+        socketConnector.send(message: "TEST")
     }
-    
-    
 }
 
